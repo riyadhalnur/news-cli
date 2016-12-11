bbdc83525df6174f9565d9e4fdad9157893c9647
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2016-12-11 16:24:45 +0600

    Remove done callbacks

--- tests/tests.js
+++ tests/tests.js
@@ -9,18 +9,18 @@ chai.use(chaiAsPromised);
 const news = require('../index.js');
 
 describe('News CLI', () => {
-  it('should get the news', (done) => {
+  it('should get the news', () => {
     let helper = news(null, {});
-    return helper.should.be.fulfilled.and.notify(done);
+    return helper.should.be.fulfilled;
   });
 
-  it('should get the top news from techcrunch by default', (done) => {
+  it('should get the top news from techcrunch by default', () => {
     let helper = news(null, {});
-    return helper.should.eventually.have.property('articles').notify(done);
+    return helper.should.eventually.have.property('articles');
   });
 
-  it('should get the latest news from hackernews', (done) => {
+  it('should get the latest news from hackernews', () => {
     let helper = news('hackernews', {sort: 'latest'});
-    return helper.should.eventually.have.property('articles').notify(done);
+    return helper.should.eventually.have.property('articles');
   });
 });
